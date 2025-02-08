9ce09326661aef33a4c63b6e8da26a2816d5d98f
Admin <madenumida@gmail.com>
2025-02-08 22:52:51 +0500

    post function

---
+++ lib/features/storage/data/firebase_storage_repo.dart
@@ -0,0 +1,66 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:mentor_hub/features/storage/domain/storage_repo.dart';
+
+class FirebaseStorageRepo implements StorageRepo {
+  final FirebaseStorage storage = FirebaseStorage.instance;
+
+  @override
+  Future<String?> uploadProfileImageMobile(String path, String fileName) {
+    return _uploadFile(path, fileName, 'profile_images');
+  }
+
+  @override
+  Future<String?> uploadProfileImageWeb(Uint8List fileBytes, String fileName) {
+    return _uploadFileBytes(fileBytes, fileName, 'profile_images');
+  }
+
+  /*
+
+  HELPER METHODS - to upload files to storage
+
+   */
+
+  // mobile platforms (file)
+  Future<String?> _uploadFile(
+      String path, String fileName, String folder) async {
+    try {
+      // get file
+      final file = File(path);
+
+      // find place to store
+      final storageRef = storage.ref().child('$folder/$fileName');
+
+      // upload
+      final uploadTask = await storageRef.putFile(file);
+
+      // get image download url
+      final downloadUrl = await uploadTask.ref.getDownloadURL();
+
+      return downloadUrl;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  // web platforms (bytes)
+  Future<String?> _uploadFileBytes(
+      Uint8List fileBytes, String fileName, String folder) async {
+    try {
+      // find place to store
+      final storageRef = storage.ref().child('$folder/$fileName');
+
+      // upload
+      final uploadTask = await storageRef.putData(fileBytes);
+
+      // get image download url
+      final downloadUrl = await uploadTask.ref.getDownloadURL();
+
+      return downloadUrl;
+    } catch (e) {
+      return null;
+    }
+  }
+}
